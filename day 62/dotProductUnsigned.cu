510ce79275d8be4b9dc215aecbf2f18559f60264
Julien Okumu <codewithjulien@gmail.com>
2025-10-01 12:56:32 +0300

    dot product kernel with unsigned ints

---
+++ day 62/dotProductUnsigned.cu
@@ -0,0 +1,30 @@
+#include <cuda_runtime.h>
+
+__global__ void dotProduct(const float* __restrict__ A, const float* __restrict__ B, float* __restrict__ result, int N) {
+	extern __shared__ float dotMem[];
+
+	unsigned int tid = threadIdx.x;
+	unsigned int idx = blockIdx.x * blockDim.x + tid;
+	unsigned int stride = blockDim.x * gridDim.x;
+
+	float tmp = 0.0f;
+
+	#pragma unroll
+	for (unsigned int i = idx; i < N; i += stride) {
+		tmp += A[idx] * B[idx];
+	}
+	dotMem[tid] = tmp;
+	__syncthreads();
+
+	#pragma unroll
+	for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+		if (tid < s) {
+			dotMem[tid] += dotMem[tid + s];
+		}
+		__syncthreads();
+	}
+
+	if (tid == 0) {
+		atomicAdd(result, dotMem[0]);
+	}
+}
