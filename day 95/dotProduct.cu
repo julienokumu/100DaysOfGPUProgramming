35bee60fab2f082d145a28d40f55a292acf7663d
Julien Okumu <codewithjulien@gmail.com>
2025-11-03 11:23:28 -0500

    Create dotProduct.cu

---
+++ day 95/dotProduct.cu
@@ -0,0 +1,38 @@
+#include <cuda_runtime.h>
+
+__global__ void dotProduct(const float* __restrict__ A, const float* __restrict__ B, float* __restrict__ result, int N) {
+    extern __shared__ float dotMem[];
+
+    int tid = threadIdx.x;
+    int idx = blockIdx.x * blockDim.x + tid;
+    int stride = blockDim.x * gridDim.x;
+
+    float tmp = 0.0f;
+
+    #pragma unroll
+    for (unsigned int i = idx; i < N; i += stride) {
+        tmp += A[i] * B[i];
+    }
+    dotMem[tid] = tmp;
+    __syncthreads();
+
+    #pragma unroll
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+        if (tid < s) {
+            dotMem[tid] += dotMem[tid + s];
+        }
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        atomicAdd(result, dotMem[0]);
+    }
+}
+
+extern "C" void solve(const float* A, const float* B, float* result, int N) {
+    int threadsPerBlock = 1024;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    size_t dotMemBytes = threadsPerBlock * sizeof(float);
+
+    dotProduct<<<blocksPerGrid, threadsPerBlock, dotMemBytes>>>(A, B, result, N);
+}
